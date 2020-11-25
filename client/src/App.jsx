9ae21e91ae0ca7cbbb249df102858b8a98d793ca
christopher garcia <garcia.christopher1986@gmail.com>
2020-11-25 17:15:04 -0500

    dailylog

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import LogIn from './Pages/SignIn/LogIn';
 import SignUp from './Pages/SignUp/SignUp';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { AppContextProvider } from './context/AppContext';
+import DailyLog from './Pages/DailyLog/DailyLog';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
             <Switch>
               <Route exact path="/login" component={LogIn} />
               <Route exact path="/signup" component={SignUp} />
+              <Route exact path="/dailylog" component={DailyLog} />
             </Switch>
           </Router>
         </AppContextProvider>
