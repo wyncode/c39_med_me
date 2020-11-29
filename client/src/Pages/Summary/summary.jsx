f50bdce6bab31ce70d7b3ecac18796e21f620ed3
nunez2020 <nunez.nunez21@gmail.com>
2020-11-29 12:21:20 -0500

    Map is functional

--- client/src/Pages/Summary/summary.jsx
+++ client/src/Pages/Summary/summary.jsx
@@ -1,29 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import ReactMapGL from 'react-map-gl';
-import mapboxgl from 'mapbox-gl';
 import './summary.css';
 
 const Summary = () => {
   const [viewport, setViewport] = useState({
-    latitude: -1.9444,
-    longitude: 30.0616,
-    zoom: 7.8,
+    latitude: 25.801354,
+    longitude: -80.203173,
+    zoom: 13,
     bearing: 0,
     pitch: 0
   });
 
   return (
     <>
-      <div className="mapbox-react">
-        <ReactMapGL
-          {...viewport}
-          width="375px"
-          height="375px"
-          mapStyle="mapbox://styles/mapbox/streets-v11"
-          onViewportChange={(nextViewport) => setViewport(nextViewport)}
-          mapboxApiAccessToken={process.env.mapboxApiAccessToken}
-        />
+      <div>
+        <div className="mapbox-react">
+          <ReactMapGL
+            {...viewport}
+            width="375px"
+            height="375px"
+            mapStyle="mapbox://styles/mapbox/streets-v11"
+            onViewportChange={(nextViewport) => setViewport(nextViewport)}
+            mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+          />
+        </div>
       </div>
 
       <div>
@@ -33,7 +34,7 @@ const Summary = () => {
             <div className="driver">
               Driver will appear on the map when order has shipped.
             </div>
-            <div className="thank">Thank you for shoping with us!</div>
+            <div className="thank">Thank you for shopping with us!</div>
             <div className="confirm">Your order confirmation: WJ19768303.</div>
           </div>
         </div>
