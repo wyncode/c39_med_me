0a70142d2d99071d7c28cd14add903874d714d4e
christianmbonet <christianm.bonet@gmail.com>
2020-12-14 20:28:44 -0500

    added med me logo, header image, and background on med profile

--- client/src/Pages/MyMedicines/MyMedicines.jsx
+++ client/src/Pages/MyMedicines/MyMedicines.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import logo from '../../Images/logo.png';
+import cabinet from '../../Images/cabinet.png';
+import '../MyMedicines/MyMedicines.css';
 
 const MyMeds = () => {
   return (
     <div>
-      <h1>hello</h1>
+      <header className="bluebox">
+        <div className="logorow">
+          <img className="logomymeds" src={logo} alt="medmelogo" />
+        </div>
+        <div className="cabinetlogorow">
+          <img className="cabinetlogo" src={cabinet} alt="cabinetlogo" />
+        </div>
+      </header>
     </div>
   );
 };
